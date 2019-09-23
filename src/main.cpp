e2aa4cd0b7ecbffc6d7010228a03d55a1b460109
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2019-09-23 16:45:00 -0600

    Parser Written in main, preparing to move to cpp and header

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,54 @@
 #include <Arduino.h>
 
-String py_data = "";
-char* string_arr[255];
 
 void setup() {
-
   Serial.begin(115200);
   pinMode(13,OUTPUT);
-
+  Serial.println("initialized");
 }
 
 void loop() {
+  String py_data = "/n";
+  String sub_string = "/n";
+  char str_arrbuff[255]; //array buffer for string conversion
+  int nums[100]; //Return array for individual int values in the string
+  int index = 0; //Index of the comma
+  int i = 0; // itterator for nums array
+  int flag = 1; //Flag for end of string object processing
+
 
   if (Serial.available() > 0){
-    py_data = Serial.read();
-    int index1 = py_data.indexOf(',');
-    py_data.toCharArray(string_arr,255);
+    Serial.println("Recieved");
+    py_data = Serial.readString();
 
+    while(flag == 1){
+
+      index = py_data.indexOf(',');
+
+      if(index == -1){//No more commas
+        py_data.toCharArray(str_arrbuff,255);
+        // len = py_data.length() - 1;
+        nums[i] = py_data.toInt();
+        //Serial.println(num);
+        i++;
+        flag = 0;
+      }
+
+      else{
+        sub_string = py_data.substring(0,index);
+        // len = sub_string.length();
+        py_data.remove(0,index+1);
+        nums[i] = sub_string.toInt();
+      // Serial.println(num);
+        i++;
+        flag = 1;
+      }
+    }
   }
 
+  if(flag == 0){
+    for(int j=0; j<i; j++){
+      Serial.println(nums[j],DEC);
+    }
+  }
 }
